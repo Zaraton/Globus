949b1f2499340c1c99e0f8e392b596399aa65ba1
Zaraton <tarakanpalkan@yandex.ru>
2019-12-09 14:12:19 +0300

    Revert "Backup_before_Android_build_fix"
    This reverts commit f0b24593ccaa9e40722b939c77ab29e4bea0cba4.

--- Assets/UI_Info_handler.cs
+++ Assets/UI_Info_handler.cs
@@ -11,7 +11,7 @@ public class UI_Info_handler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Wrapper.SetActive(false);
+        Close_Object_Info();
     }
 
     // Update is called once per frame
