0692df24882e3aa66a00f8fb47931b30bc943572
Grigory Khromov <38518245+Grihalkh@users.noreply.github.com>
2020-02-06 16:46:51 +0300

    x100 button rework done
    Now it's a pretty clock :)

---
+++ Assets/SpeedUpButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedUpButton : MonoBehaviour
+{
+    public GameObject ClockHand;
+    public Toggle ToggleScript;
+
+    public Image Image;
+    public Sprite ImgOn, ImgOff;
+
+    public float lowSpeed = 1.0f;
+    public float highSpeed = 100.0f;
+
+    bool is100 = false;
+
+    float currentSpeed;
+    float angle = 10.0f;
+
+    SpeedUpButton()
+    {
+        currentSpeed = lowSpeed;
+    }
+
+    void Update()
+    {
+        if (ClockHand != null)
+            ClockHand.transform.Rotate(-Vector3.forward * Time.deltaTime * angle * currentSpeed); // поворачиваем стрелку
+
+        if (ToggleScript.isOn ^ is100) // если эти переменные разные, состояние изменилось -> запускаем ToggleClock()
+            ToggleClock();
+    }
+
+    void ToggleClock()
+    {
+        is100 = !is100;
+
+        if (is100) // время ускорено
+        {
+            Image.sprite = ImgOn;
+            currentSpeed = highSpeed;
+        }
+        else // время нормальное
+        {
+            Image.sprite = ImgOff;
+            currentSpeed = lowSpeed;
+        }
+    }
+
+}
