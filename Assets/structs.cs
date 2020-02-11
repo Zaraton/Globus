5a24db8e94d3f48b12ac3a727353afd636d43e66
Colon1al <38556136+Colon1al@users.noreply.github.com>
2020-02-11 21:18:08 +0300

    Sat data class done

--- Assets/structs.cs
+++ Assets/structs.cs
@@ -11,6 +11,7 @@ public class SpaceportList
        this.SpList = list;
    }
 }
+
 [Serializable]
 public class Spaceport
 {
@@ -28,6 +29,34 @@ public class Spaceport
         this.LocationName = locationName;
         this.Latitude = latitude;
         this.Longitude = longitude;
+        this.Info = info;
+    }
+}
+
+[Serializable]
+public class SatelliteList
+{
+   public List<Satellite> SList;
+   public SatelliteList(List<Satellite> list)
+   {
+       this.SList = list;
+   }
+}
+[Serializable]
+public class Satellite
+{
+    public string Name;
+    public string tle1;
+    public string tle2;
+    public string Type;
+    public string Info;
+
+    public Satellite(string name, string tle_1, string tle_2, string type,string info)
+    {
+        this.Name = name;
+        this.tle1 = tle_1;
+        this.tle2 = tle_2;
+        this.Type = type;
         this.Info = info;
     }
 }
